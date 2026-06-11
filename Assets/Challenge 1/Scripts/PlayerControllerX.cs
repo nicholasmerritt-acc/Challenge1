d27e59ba5d8b58bc61bd30eb3cfea215d0603cea
nicholasmerritt-acc <nicholas.merritt@g.austincc.edu>
2026-06-11 14:41:52 -0500

    start over on obstacle collision
    make the game more fun by setting the player back at the start when they hit an obstacle

--- Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -6,10 +6,12 @@ public class PlayerControllerX : MonoBehaviour
     private float rotationSpeed = 100f;
     private float verticalInput;
 
+    private Vector3 startPosition;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        startPosition = transform.position;
     }
 
     // Update is called once per frame
@@ -23,5 +25,14 @@ public class PlayerControllerX : MonoBehaviour
 
         // tilt the plane up/down based on up/down arrow keys
         transform.Rotate(Vector3.left * rotationSpeed * Time.deltaTime * verticalInput);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            Debug.Log("Game Over, Man!");
+            transform.SetPositionAndRotation(startPosition, Quaternion.identity);
+        }
     }
 }
